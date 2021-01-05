f9dae242fe9d5bdc33f0dbcb5153a72b49282dc1
jhonmcc <jhonmcc@gmail.com>
2021-01-05 15:48:58 -0300

    adicionado classe exemplo de comportamento da classe

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # javascript-learn
+
+* Requisitos para interpretação do javascript 
+- instalado o nodejs a partir da versao 13, ele permite trabalhar com modularização do js.
+    - caso precise intercalar entre verssões deixarei um link sobre como fazer isso logo abaixo
+        - :) logo mais jogo o link aqui 
+        
+- vscode ou editor de texto do seu agrado.
+
---
+++ js-orientado-objeto/add-comportamento.js
@@ -0,0 +1,28 @@
+// adicionando comportamento a classe também conhecidos como metodos da classe
+
+class Carro{
+    cor;
+    ano;
+    acao;
+
+    liga_desliga(valor){
+        this.acao = valor;
+    }
+
+    acelera(){
+        if (this.acao == true){
+            console.log('vruummm');
+        }
+        else{
+            console.log('o carro esta desligado');
+        }
+    }
+}
+
+const fusca = new Carro();
+fusca.cor = 'amarelo';
+fusca.ano = 1990;
+fusca.acelera();
+
+fusca.liga_desliga(true);
+fusca.acelera();
