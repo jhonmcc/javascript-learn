e5d92768e04a64b5ea875cf0859d71faca35e217
jhonmcc <jhonmcc@gmail.com>
2021-01-07 14:13:49 -0300

    criado exemplo de modularizacao com js

--- js-orientado-objeto/4-modularizacao.js
+++ js-orientado-objeto/4-modularizacao.js
@@ -1,1 +1,15 @@
-// dar sequencia em modularizacao
+// para usar modularizacao teras que dar um npm init para criar o arquivo package.json e acrescentar o type
+// veja o arquivo de referencia package.json na linha após o campo "license" acrescente "type": "module",
+// utilize o npm init -y caso queira pular os steps de definicao do projeto
+
+// importando um modulo
+import { Cliente } from "./4.1-moduloCliente.js";
+
+// utilizando o import
+const testCliente = new Cliente();
+
+testCliente.nome = "Teste Nome";
+testCliente.idade = 99;
+testCliente.saudacao();
+
+// node 4-modularizacao.js para realizar o teste
---
+++ js-orientado-objeto/4.1-moduloCliente.js
@@ -0,0 +1,8 @@
+export class Cliente {
+    nome = ''
+    idade = 0
+
+    saudacao(){
+        console.log('Oii ' + this.nome);
+    }
+}
