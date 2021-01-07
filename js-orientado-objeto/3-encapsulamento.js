66dfd9cf0d9918dcde5985dfa2451458f4f9c96b
jhonmcc <jhonmcc@gmail.com>
2021-01-07 15:47:40 -0300

    utilizando getters e setters encapsulando atributos

--- js-orientado-objeto/3-encapsulamento.js
+++ js-orientado-objeto/3-encapsulamento.js
@@ -10,6 +10,14 @@ class Conta{
     getSaldo(){
         return this.#saldo
     }
+
+    get saldo(){
+        return this.#saldo
+    }
+
+    set saldo(value){
+        this.#saldo = value;
+    }
 }
 
 const contaTeste = new Conta();
@@ -17,5 +25,12 @@ contaTeste.agencia = 'test';
 // contaTeste.#saldo = 0 nao da pra manipular assim é necessário ter setters e getters
 contaTeste.setSaldo(123);
 console.log(contaTeste)
-console.log(contaTeste.getSaldo());
+console.log('Saldo: ', contaTeste.getSaldo());
+
+// testando getters e setters
+const conta2 = new Conta();
+conta2.agencia = "ag 2";
+conta2.saldo = 321
+console.log(conta2);
+console.log('Saldo: ', conta2.saldo)
 
